6197174f30e6366ea8340e004dc7eb2c50baa8a3
KTV02 <l.kremp@gmx.de>
2020-09-04 17:06:19 +0200

    added remover.py which uses text file of duplicates created by removeDuplicates to delete duplicate images

--- remover.py
+++ remover.py
@@ -1,72 +1,32 @@
 # dupFinder.py
 import os, sys
 import hashlib
+from time import sleep
  
-def findDup(parentFolder):
-    # Dups in format {hash:[names]}
-    dups = {}
-    for dirName, subdirs, fileList in os.walk(parentFolder):
-        print('Scanning %s...' % dirName)
-        for filename in fileList:
-            # Get the path to the file
-            path = os.path.join(dirName, filename)
-            # Calculate hash
-            file_hash = hashfile(path)
-            # Add or append the file path
-            if file_hash in dups:
-                dups[file_hash].append(path)
-            else:
-                dups[file_hash] = [path]
-    return dups
- 
- 
-# Joins two dictionaries
-def joinDicts(dict1, dict2):
-    for key in dict2.keys():
-        if key in dict1:
-            dict1[key] = dict1[key] + dict2[key]
-        else:
-            dict1[key] = dict2[key]
- 
- 
-def hashfile(path, blocksize = 65536):
-    afile = open(path, 'rb')
-    hasher = hashlib.md5()
-    buf = afile.read(blocksize)
-    while len(buf) > 0:
-        hasher.update(buf)
-        buf = afile.read(blocksize)
-    afile.close()
-    return hasher.hexdigest()
- 
- 
-def printResults(dict1):
-    results = list(filter(lambda x: len(x) > 1, dict1.values()))
-    if len(results) > 0:
-        print('Duplicates Found:')
-        print('The following files are identical. The name could differ, but the content is identical')
-        print('___________________')
-        for result in results:
-            for subresult in result:
-                print('\t\t%s' % subresult)
-            print('___________________')
- 
-    else:
-        print('No duplicate files found.')
- 
- 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        dups = {}
-        folders = sys.argv[1:]
-        for i in folders:
-            # Iterate the folders given
-            if os.path.exists(i):
-                # Find the duplicated files and append them to the dups
-                joinDicts(dups, findDup(i))
-            else:
-                print('%s is not a valid path, please verify' % i)
-                sys.exit()
-        printResults(dups)
-    else:
-        print('Usage: python dupFinder.py folder or python dupFinder.py folder1 folder2 folder3')
+def opener():
+    lines=[]
+    file=open("imageDuplicates.txt","r")
+    for line in file:
+        lines.append(line)
+    return lines
+
+
+def remove(lines):
+    deleted=[]
+    for path in lines:
+        url1,url2=path.split("and")
+        url1=url1.replace("Duplicate found:","")
+        url2=url2.replace("and","")
+        url1=url1.replace(" ","")
+        url2=url2.replace(" ","")
+        url2=url2.replace("\n","")
+        if not url1 in deleted and not url2 in deleted:
+            deleted.append(url1)
+            #print("deleted",url1)
+            #print("total",deleted)
+            try:
+                os.remove(url1)
+            except:
+                continue
+
+remove(opener())
